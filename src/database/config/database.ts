872b66970fb2ec653191894cfaba1e971425c014
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-02 15:31:34 -0300

    fixing config

--- src/database/config/database.ts
+++ src/database/config/database.ts
@@ -1,9 +1,9 @@
 import { Options } from 'sequelize';
 
 const config: Options = {
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: 'Library',
   host: process.env.DB_HOST || 'localhost',
   port: Number(process.env.DB_PORT) || 3306,
   dialect: 'mysql',
