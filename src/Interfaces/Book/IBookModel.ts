6000b7ae7fceeff1243a182a5f0d6563beda31ad
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-05 16:58:40 -0300

    correcting the interface findById method

--- src/Interfaces/Book/IBookModel.ts
+++ src/Interfaces/Book/IBookModel.ts
@@ -2,5 +2,5 @@ import IBook from "./IBook";
 
 export default interface IBookModel {
     findAll(): Promise<IBook[]>;
-    findById(): Promise<IBook>;
+    findById(id: number): Promise<IBook | null>;
 }
