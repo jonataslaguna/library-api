b83852ffa563bfa29214f720b005e0dbb5551697
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-05 17:00:25 -0300

    correcting the model findAll method

--- src/models/BookModel.ts
+++ src/models/BookModel.ts
@@ -2,7 +2,9 @@ import SequelizeBook from "../database/models/SequelizeBook";
 import { IBook, IBookModel } from "../Interfaces/Book";
 
 export default class BookModel implements IBookModel {
-    public findAll(): Promise<IBook[]> {
-        return SequelizeBook.findAll();
+ 
+    async findAll(): Promise<IBook[]> {
+        const books = await SequelizeBook.findAll();
+        return books;
     }
 }
