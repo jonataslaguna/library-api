b0021a6c5a0f747b04969d4d73fbdac220a3b6d3
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-05 17:15:34 -0300

    implemented endpoint to find book by id

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -7,4 +7,6 @@ const router = Router();
 
 router.get('/', (req: Request, res: Response) => bookController.findAll(req, res));
 
+router.get('/:id', (req: Request, res: Response) => bookController.findById(req, res));
+
 export default router;
