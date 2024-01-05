2411d38518d66ab9f5caf4ad257dea96d4457f02
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-05 17:07:34 -0300

    update BookModel

--- src/models/BookModel.ts
+++ src/models/BookModel.ts
@@ -2,14 +2,15 @@ import SequelizeBook from "../database/models/SequelizeBook";
 import { IBook, IBookModel } from "../Interfaces/Book";
 
 export default class BookModel implements IBookModel {
+    private model = SequelizeBook;
  
     async findAll(): Promise<IBook[]> {
-        const books = await SequelizeBook.findAll();
+        const books = await this.model.findAll();
         return books;
     }
 
     async findById(id: number): Promise<IBook | null> {
-        const book = await SequelizeBook.findByPk(id);
+        const book = await this.model.findByPk(id);
         return book;
     }
 }
