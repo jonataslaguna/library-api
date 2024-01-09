d5dd3c3eb2bdd34c2e78d02a152b542c21471574
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-09 14:39:10 -0300

    implemented method to find book for title in Interface IBookModel

--- src/Interfaces/Book/IBookModel.ts
+++ src/Interfaces/Book/IBookModel.ts
@@ -4,4 +4,5 @@ export default interface IBookModel {
     findAll(): Promise<IBook[]>;
     findById(id: number): Promise<IBook | null>;
     createBook(newBookData: IBook): Promise<IBook>; 
+    findByTitle(title: string): Promise<IBook | null>;
 }
