4bef1a7693f64fd14306abf058dd2ebba04f6ec7
Jonatas Laguna <jonataslaguna.js@gmail.com>
2024-01-09 14:41:35 -0300

    added validation in endpoit post

--- src/routes/book.routes.ts
+++ src/routes/book.routes.ts
@@ -1,5 +1,6 @@
 import { Request, Router, Response } from "express";
 import BookController from '../controllers/BookController';
+import BookValidation from '../middlewares/inputValidations/BookValidation';
 
 const bookController = new BookController();
 
@@ -9,6 +10,6 @@ router.get('/', (req: Request, res: Response) => bookController.findAll(req, res
 
 router.get('/:id', (req: Request, res: Response) => bookController.findById(req, res));
 
-router.post('/', (req: Request, res: Response) => bookController.createBook(req, res));
+router.post('/', BookValidation.validateNewBook, (req: Request, res: Response) => bookController.createBook(req, res));
 
 export default router;
